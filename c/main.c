f25d45c89358a60f1830937e3177ac26c722a0b2
Damien Leroy <damien7leroy@gmail.com>
2017-05-12 14:28:48 +0200

    Refactorize C version

--- c/main.c
+++ c/main.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #include <windows.h>
 
-void dir (char *path);
-void dir_r(char *path, int level);
+void dir (char *path, int level);
 void tab(int level);
 
 int main (int argc, char *argv[])
 {
   printf("Fonction windows DIR /S en C\n\n");
 
+  // Appel de l'initialisation de la fonction dir récursive avec le chemin du dossier cible
   if (argc != 2) {
-    dir(".");
+    dir(".", 0); // "." si aucun chemin n'est spécifié en paramètre
   }
   else {
-    dir(argv[1]);
+    dir(argv[1], 0); // le chemin spécifié sinon
   }
 
   return 0;
 }
 
-void dir (char *path) {
-
-  // Le volume dans le lecteur C s'appelle OS
-  // Le numéro de série du volume est XXXX-XXXX
-
-  printf(" Repertoire de %s\n", path); // Chemin relatif à remplacer par absolu
-
+void dir(char *path, int level) {
   WIN32_FIND_DATA FindFileData;
   HANDLE hFind;
   LARGE_INTEGER filesize;
@@ -37,88 +31,37 @@ void dir (char *path) {
   strcpy(szDir, path);
   strcat(szDir, TEXT("\\*"));
 
+  // Find first file (current dir)
   hFind = FindFirstFile(szDir, &FindFileData);
 
-  int nFile = 0;
-  int nDir = 0;
-  int fileSpace = 0;
-  int freeSpace = 0;
-
   do {
+    // Format creation date of the file
     FileTimeToLocalFileTime(&FindFileData.ftLastWriteTime, &FindFileData.ftLastWriteTime);
     FileTimeToSystemTime(&FindFileData.ftLastWriteTime, &st);
     GetDateFormat(LOCALE_USER_DEFAULT, DATE_SHORTDATE, &st, NULL, szLocalDate, 255);
     GetTimeFormat(LOCALE_USER_DEFAULT, TIME_NOSECONDS, &st, NULL, szLocalTime, 255);
-    if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-      printf("%s  %s    <DIR>          %s\n", szLocalDate, szLocalTime, FindFileData.cFileName);
-      nDir++;
-      if (strcmp(FindFileData.cFileName, ".") != 0 && strcmp(FindFileData.cFileName, "..") != 0) {
-        char new_path [255];
-        strcpy(new_path, path);
-        strcat(new_path, "\\");
-        strcat(new_path, FindFileData.cFileName);
-        dir_r(new_path, 1);
-      }
-    }
-    else {
-      filesize.LowPart = FindFileData.nFileSizeLow;
-      filesize.HighPart = FindFileData.nFileSizeHigh;
-      printf("%s  %s %*lld %s\n", szLocalDate, szLocalTime, 17, filesize.QuadPart, FindFileData.cFileName);
-      nFile++;
-      fileSpace += filesize.QuadPart;
-    }
-  }
-  while (FindNextFile(hFind, &FindFileData) != 0);
 
-  printf("%*d fichier(s)     %d octets\n", 15, nFile, fileSpace);
-  printf("%*d Rep(s)     %d octets libres\n", 15, nDir, freeSpace);
-
-  FindClose(hFind);
-}
-
-
-void dir_r(char *path, int level) {
-  WIN32_FIND_DATA FindFileData;
-  HANDLE hFind;
-  LARGE_INTEGER filesize;
-  TCHAR szDir[MAX_PATH];
-
-  SYSTEMTIME st;
-  char szLocalDate[255], szLocalTime[255];
-
-  strcpy(szDir, path);
-  strcat(szDir, TEXT("\\*"));
-
-  hFind = FindFirstFile(szDir, &FindFileData);
-
-  int nFile = 0;
-  int nDir = 0;
-  int fileSpace = 0;
-  int freeSpace = 0;
-
-  do {
-    FileTimeToLocalFileTime(&FindFileData.ftLastWriteTime, &FindFileData.ftLastWriteTime);
-    FileTimeToSystemTime(&FindFileData.ftLastWriteTime, &st);
-    GetDateFormat(LOCALE_USER_DEFAULT, DATE_SHORTDATE, &st, NULL, szLocalDate, 255);
-    GetTimeFormat(LOCALE_USER_DEFAULT, TIME_NOSECONDS, &st, NULL, szLocalTime, 255);
+    // Print tab with recursivity level
     tab(level);
+
     if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+      // Print directory informations
       printf("%s  %s    <DIR>          %s\n", szLocalDate, szLocalTime, FindFileData.cFileName);
-      nDir++;
+
+      // If not . or .. directory, call recursivity
       if (strcmp(FindFileData.cFileName, ".") != 0 && strcmp(FindFileData.cFileName, "..") != 0) {
         char new_path [255];
         strcpy(new_path, path);
         strcat(new_path, "\\");
         strcat(new_path, FindFileData.cFileName);
-        dir_r(new_path, level + 1);
+        dir(new_path, level + 1);
       }
     }
     else {
+      // Print file informations
       filesize.LowPart = FindFileData.nFileSizeLow;
       filesize.HighPart = FindFileData.nFileSizeHigh;
       printf("%s  %s %*lld %s\n", szLocalDate, szLocalTime, 17, filesize.QuadPart, FindFileData.cFileName);
-      nFile++;
-      fileSpace += filesize.QuadPart;
     }
   }
   while (FindNextFile(hFind, &FindFileData) != 0);
